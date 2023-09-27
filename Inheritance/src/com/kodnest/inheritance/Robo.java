ff44d9a3013ee169825fe29adc1734bab08a2e59
Basavaraj Aili <basavarajaili515@gmail.com>
2023-09-27 15:04:09 +0530

    Inheritance in Java
    New Problems added

---
+++ Inheritance/src/com/kodnest/inheritance/Robo.java
@@ -0,0 +1,18 @@
+package com.kodnest.inheritance;
+
+public class Robo {
+	String name;
+	String type;
+	
+	public void talk() {
+		System.out.println(name+" Robo is talking..");
+	}
+	
+	public void walk() {
+		System.out.println(name+" Robo is walking..");
+	}
+	
+	public void charge() {
+		System.out.println(name+" Robo is fast charging..");
+	}
+}
